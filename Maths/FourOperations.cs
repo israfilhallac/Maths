ca794d0caef4a1c4bb205dcf72999a25b6d66f80
israfilhallac <i.hallac1408@gmail.com>
2021-02-21 13:18:04 +0200

    Add project files.

---
+++ Maths/FourOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maths
+{
+    class FourOperations
+    {
+        public void Gather(int number1,int number2)
+        {
+            int total = number1 + number2;
+            Console.WriteLine("Sonuc :" +total);
+        }
+        public void  Sticker(int number1,int number2)
+        {
+            int result = number1 - number2;
+            Console.WriteLine("Sonuc :" + result);
+        }
+        public void Multiplication(int number1, int number2)
+        {
+            int result = number1 * number2;
+            Console.WriteLine("Sonuc :" + result);
+        }
+
+        public void Split(int number1, int number2)
+        {
+            int result = number1 / number2;
+            Console.WriteLine("Sonuc :" + result);
+        }
+    }
+}
---
+++ Maths/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Maths
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            FourOperations fourOperations = new FourOperations();
+            fourOperations.Gather(5, 23);
+            fourOperations.Sticker(20, 6);
+            fourOperations.Multiplication(5, 6);
+            fourOperations.Split(42,6);
+
+
+        }
+    }
+}
